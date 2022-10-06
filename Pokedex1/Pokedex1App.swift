6bb8be95b334ade1ca87aef93f45f4e5f1517c58
mcantop <maciejcantop@icloud.com>
2022-10-07 01:18:40 +0200

    Initial Commit

---
+++ Pokedex1/Pokedex1App.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex1App.swift
+//  Pokedex1
+//
+//  Created by Maciej on 07/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
